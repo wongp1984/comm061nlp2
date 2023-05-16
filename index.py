fb893df941e6a8d94373a131fbd75d3cf61e2ed2
Alvin Kwong <sk02039@surrey.ac.uk>
2023-05-16 22:36:54 +0100

    modified the index.html and add the run_app.ipynb

--- index.py
+++ index.py
@@ -182,4 +182,5 @@ def predict_emotion():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=80)
+    # app.run(host='0.0.0.0', debug=True, port=80)
+    app.run(host='127.0.0.1', debug=True)
